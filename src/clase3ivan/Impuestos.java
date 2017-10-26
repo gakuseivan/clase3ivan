2481f9e3a1f7a07eea9eec073f5c004ab70d01eb
gakuseivan <gakuseivan@gmail.com>
2017-10-26 20:31:51 -0300

    Esta clase calcula si debe pagar impuestos

---
+++ src/clase3ivan/Impuestos.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase3ivan;
+import java.util.Scanner;
+
+/**
+ *
+ * @author sala306
+ */
+public class Impuestos 
+{
+   public static void main(String[] args)
+   {
+     int sueldo;
+     
+     Scanner escaner = new Scanner(System.in);
+     System.out.println("Ingrese su salario: ");
+     sueldo = escaner.nextInt();
+     
+       if (sueldo>600000) 
+       {
+         System.out.println("Debe Pagar Impuestos");  
+       }
+       
+       
+   }
+                      
+}
